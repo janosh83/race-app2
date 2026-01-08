e738222029d4594c456c1e06b117f375efddbe06
Zdenek Jancik <zdenek.jancik@gmail.com>
2026-01-08 20:59:29 +0100

    Fixed possible side effects with inline logging

--- my-frontend/src/App.js
+++ my-frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import ActiveRacePage from './components/Pages/ActiveRacePage';
 import AdminLayout from './components/Layouts/AdminLayout';
@@ -14,11 +14,22 @@ import { TimeProvider } from './contexts/TimeContext';
 import { isTokenExpired } from './utils/api';
 import { logger } from './utils/logger';
 
+// Logs once on mount to avoid side effects during render
+const LogOnce = ({ message, data, children }) => {
+  useEffect(() => {
+    logger.info('ROUTING', message, data);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return children;
+};
+
 function App() {
   const token = localStorage.getItem('accessToken');
   const isLoggedIn = token && !isTokenExpired(token);
-  
-  logger.info('ROUTING', 'App initialized', { isLoggedIn, hasToken: !!token, tokenExpired: token ? isTokenExpired(token) : 'no-token' });
+
+  useEffect(() => {
+    logger.info('ROUTING', 'App initialized', { isLoggedIn, hasToken: !!token, tokenExpired: token ? isTokenExpired(token) : 'no-token' });
+  }, [isLoggedIn, token]);
   
   return (
     <TimeProvider>
@@ -26,93 +37,79 @@ function App() {
         <Routes>
           {/* Auth routes (public) */}
           <Route path="/login" element={!isLoggedIn ? (
-            <>
-              {logger.info('ROUTING', 'User accessing login route', { isLoggedIn })}
+            <LogOnce message="User accessing login route" data={{ isLoggedIn }}>
               <Login />
-            </>
+            </LogOnce>
           ) : (
-            <>
-              {logger.info('ROUTING', 'User already logged in, redirecting from login to /race')}
+            <LogOnce message="User already logged in, redirecting from login to /race" data={{ isLoggedIn }}>
               <Navigate to="/race" />
-            </>
+            </LogOnce>
           )} />
           <Route path="/forgot-password" element={!isLoggedIn ? (
-            <>
-              {logger.info('ROUTING', 'User accessing forgot-password route', { isLoggedIn })}
+            <LogOnce message="User accessing forgot-password route" data={{ isLoggedIn }}>
               <ForgotPassword />
-            </>
+            </LogOnce>
           ) : (
-            <>
-              {logger.info('ROUTING', 'User already logged in, redirecting from forgot-password to /race')}
+            <LogOnce message="User already logged in, redirecting from forgot-password to /race" data={{ isLoggedIn }}>
               <Navigate to="/race" />
-            </>
+            </LogOnce>
           )} />
           <Route path="/reset-password" element={!isLoggedIn ? (
-            <>
-              {logger.info('ROUTING', 'User accessing reset-password route', { isLoggedIn })}
+            <LogOnce message="User accessing reset-password route" data={{ isLoggedIn }}>
               <ResetPassword />
-            </>
+            </LogOnce>
           ) : (
-            <>
-              {logger.info('ROUTING', 'User already logged in, redirecting from reset-password to /race')}
+            <LogOnce message="User already logged in, redirecting from reset-password to /race" data={{ isLoggedIn }}>
               <Navigate to="/race" />
-            </>
+            </LogOnce>
           )} />
 
           {/* Race routes (protected) */}
           <Route path="/race" element={isLoggedIn ? (
-            <>
-              {logger.info('ROUTING', 'User accessing protected race routes', { isLoggedIn })}
+            <LogOnce message="User accessing protected race routes" data={{ isLoggedIn }}>
               <RaceLayout />
-            </>
+            </LogOnce>
           ) : (
-            <>
-              {logger.info('ROUTING', 'User not logged in, redirecting to login')}
+            <LogOnce message="User not logged in, redirecting to login" data={{ isLoggedIn }}>
               <Navigate to="/login" />
-            </>
+            </LogOnce>
           )}>
             <Route index element={<ActiveRacePage />} />
             <Route path=":raceId/map" element={
-              <>
-                {logger.info('ROUTING', 'User accessing map page')}
+              <LogOnce message="User accessing map page" data={{ isLoggedIn }}>
                 <MapPage />
-              </>
+              </LogOnce>
             } />
             <Route path=":raceId/tasks" element={
-              <>
-                {logger.info('ROUTING', 'User accessing tasks page')}
+              <LogOnce message="User accessing tasks page" data={{ isLoggedIn }}>
                 <TasksPage />
-              </>
+              </LogOnce>
             } />
             <Route path=":raceId/standings" element={
-              <>
-                {logger.info('ROUTING', 'User accessing standings page')}
+              <LogOnce message="User accessing standings page" data={{ isLoggedIn }}>
                 <StandingsPage />
-              </>
+              </LogOnce>
             } />
           </Route>
 
           {/* Admin routes (protected) */}
           <Route path="/admin" element={isLoggedIn ? (
-            <>
-              {logger.info('ROUTING', 'User accessing protected admin routes', { isLoggedIn })}
+            <LogOnce message="User accessing protected admin routes" data={{ isLoggedIn }}>
               <AdminLayout />
-            </>
+            </LogOnce>
           ) : (
-            <>
-              {logger.info('ROUTING', 'User not logged in, redirecting to login')}
+            <LogOnce message="User not logged in, redirecting to login" data={{ isLoggedIn }}>
               <Navigate to="/login" />
-            </>
+            </LogOnce>
           )}>
             <Route index element={<AdminPage />} />
           </Route>
 
           {/* Catch-all redirect */}
           <Route path="*" element={
-            <>
-              {logger.info('ROUTING', 'User accessing unknown route, redirecting', { isLoggedIn, redirectTo: isLoggedIn ? "/race" : "/login" })}
+            <LogOnce message="User accessing unknown route, redirecting" data={{ isLoggedIn, redirectTo: isLoggedIn ? "/race" : "/login" }}>
               <Navigate to={isLoggedIn ? "/race" : "/login"} />
-            </>
+            </LogOnce>
           } />
         </Routes>
       </Router>
