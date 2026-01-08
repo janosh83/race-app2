5a02d976607a5ad29078dc6c8c8c1480eeaca821
Zdenek Jancik <zdenek.jancik@gmail.com>
2026-01-08 20:42:04 +0100

    Added logging of the routing

--- my-frontend/src/App.js
+++ my-frontend/src/App.js
@@ -18,32 +18,102 @@ function App() {
   const token = localStorage.getItem('accessToken');
   const isLoggedIn = token && !isTokenExpired(token);
   
-  logger.info('COMPONENT', 'App initialized', { isLoggedIn, hasToken: !!token });
+  logger.info('ROUTING', 'App initialized', { isLoggedIn, hasToken: !!token, tokenExpired: token ? isTokenExpired(token) : 'no-token' });
   
   return (
     <TimeProvider>
       <Router>
         <Routes>
           {/* Auth routes (public) */}
-          <Route path="/login" element={!isLoggedIn ? <Login /> : <Navigate to="/race" />} />
-          <Route path="/forgot-password" element={!isLoggedIn ? <ForgotPassword /> : <Navigate to="/race" />} />
-          <Route path="/reset-password" element={!isLoggedIn ? <ResetPassword /> : <Navigate to="/race" />} />
+          <Route path="/login" element={!isLoggedIn ? (
+            <>
+              {logger.info('ROUTING', 'User accessing login route', { isLoggedIn })}
+              <Login />
+            </>
+          ) : (
+            <>
+              {logger.info('ROUTING', 'User already logged in, redirecting from login to /race')}
+              <Navigate to="/race" />
+            </>
+          )} />
+          <Route path="/forgot-password" element={!isLoggedIn ? (
+            <>
+              {logger.info('ROUTING', 'User accessing forgot-password route', { isLoggedIn })}
+              <ForgotPassword />
+            </>
+          ) : (
+            <>
+              {logger.info('ROUTING', 'User already logged in, redirecting from forgot-password to /race')}
+              <Navigate to="/race" />
+            </>
+          )} />
+          <Route path="/reset-password" element={!isLoggedIn ? (
+            <>
+              {logger.info('ROUTING', 'User accessing reset-password route', { isLoggedIn })}
+              <ResetPassword />
+            </>
+          ) : (
+            <>
+              {logger.info('ROUTING', 'User already logged in, redirecting from reset-password to /race')}
+              <Navigate to="/race" />
+            </>
+          )} />
 
           {/* Race routes (protected) */}
-          <Route path="/race" element={isLoggedIn ? <RaceLayout /> : <Navigate to="/login" />}>
+          <Route path="/race" element={isLoggedIn ? (
+            <>
+              {logger.info('ROUTING', 'User accessing protected race routes', { isLoggedIn })}
+              <RaceLayout />
+            </>
+          ) : (
+            <>
+              {logger.info('ROUTING', 'User not logged in, redirecting to login')}
+              <Navigate to="/login" />
+            </>
+          )}>
             <Route index element={<ActiveRacePage />} />
-            <Route path=":raceId/map" element={<MapPage />} />
-            <Route path=":raceId/tasks" element={<TasksPage />} />
-            <Route path=":raceId/standings" element={<StandingsPage />} />
+            <Route path=":raceId/map" element={
+              <>
+                {logger.info('ROUTING', 'User accessing map page')}
+                <MapPage />
+              </>
+            } />
+            <Route path=":raceId/tasks" element={
+              <>
+                {logger.info('ROUTING', 'User accessing tasks page')}
+                <TasksPage />
+              </>
+            } />
+            <Route path=":raceId/standings" element={
+              <>
+                {logger.info('ROUTING', 'User accessing standings page')}
+                <StandingsPage />
+              </>
+            } />
           </Route>
 
           {/* Admin routes (protected) */}
-          <Route path="/admin" element={isLoggedIn ? <AdminLayout /> : <Navigate to="/login" />}>
+          <Route path="/admin" element={isLoggedIn ? (
+            <>
+              {logger.info('ROUTING', 'User accessing protected admin routes', { isLoggedIn })}
+              <AdminLayout />
+            </>
+          ) : (
+            <>
+              {logger.info('ROUTING', 'User not logged in, redirecting to login')}
+              <Navigate to="/login" />
+            </>
+          )}>
             <Route index element={<AdminPage />} />
           </Route>
 
           {/* Catch-all redirect */}
-          <Route path="*" element={<Navigate to={isLoggedIn ? "/race" : "/login"} />} />
+          <Route path="*" element={
+            <>
+              {logger.info('ROUTING', 'User accessing unknown route, redirecting', { isLoggedIn, redirectTo: isLoggedIn ? "/race" : "/login" })}
+              <Navigate to={isLoggedIn ? "/race" : "/login"} />
+            </>
+          } />
         </Routes>
       </Router>
     </TimeProvider>
