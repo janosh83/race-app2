bedd232ee1c784c064544e742dba9c7d78a4f662
Zdenek Jancik <zdenek.jancik@gmail.com>
2026-01-09 20:17:15 +0100

    Added variable REACT_APP_DEBUG_MODE to switch on frontend logging even when in prod

--- my-frontend/src/utils/logger.js
+++ my-frontend/src/utils/logger.js
@@ -1,9 +1,12 @@
 /**
  * Logger utility for consistent logging across the frontend
  * Provides structured logging with categories and log levels
+ * 
+ * Debug mode controlled by REACT_APP_DEBUG_MODE env variable (true/false)
+ * Can be set on Render even in production to enable/disable debug output
  */
 
-const isDev = true;
+const isDev = process.env.REACT_APP_DEBUG_MODE === 'true';
 
 // Color codes for console output
 const colors = {
